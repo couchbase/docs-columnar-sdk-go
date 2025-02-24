54b7e578b1c3f9bd2a1f88a88ba0258d48959349
Charles Dixon <chvckd@gmail.com>
2025-02-24 15:13:25 +0000

    Update examples to use Go

---
+++ modules/hello-world/examples/helloworld.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/couchbaselabs/gocbcolumnar"
+)
+
+func main() {
+	connStr := "couchbases://..."
+	username := "..."
+	password := "..."
+
+	cluster, err := cbcolumnar.NewCluster(
+		connStr,
+		cbcolumnar.NewCredential(username, password),
+		// The third parameter is optional.
+		// This example sets the default server query timeout to 3 minutes,
+		// that is the timeout value sent to the query server.
+		cbcolumnar.NewClusterOptions().SetTimeoutOptions(
+			cbcolumnar.NewTimeoutOptions().SetServerQueryTimeout(3*time.Minute),
+		),
+	)
+	handleErr(err)
+
+	// We create a new context with a timeout of 2 minutes.
+	// This context will apply timeout/cancellation on the client side.
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Minute)
+	defer cancel()
+
+	printRows := func(result *cbcolumnar.QueryResult) {
+		for row := result.NextRow(); row != nil; row = result.NextRow() {
+			var content map[string]interface{}
+
+			err = row.ContentAs(&content)
+			handleErr(err)
+
+			fmt.Printf("Got row content: %v", content)
+		}
+	}
+
+	// Execute a query and process rows as they arrive from server.
+	// Results are always streamed from the server.
+	result, err := cluster.ExecuteQuery(ctx, "select 1")
+	handleErr(err)
+
+	printRows(result)
+
+	// Execute a query with positional arguments.
+	result, err = cluster.ExecuteQuery(
+		ctx,
+		"select ?=1",
+		cbcolumnar.NewQueryOptions().SetPositionalParameters([]interface{}{1}),
+	)
+	handleErr(err)
+
+	printRows(result)
+
+	// Execute a query with named arguments.
+	result, err = cluster.ExecuteQuery(
+		ctx,
+		"select $foo=1",
+		cbcolumnar.NewQueryOptions().SetNamedParameters(map[string]interface{}{"foo": 1}),
+	)
+	handleErr(err)
+
+	printRows(result)
+
+	err = cluster.Close()
+	handleErr(err)
+}
+
+func handleErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
